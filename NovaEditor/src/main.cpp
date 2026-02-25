9879dff59bed70b73a000e5ee86b6bc7acf1de8c
m4reQ <m.mizerski2004@gmail.com>
2026-02-25 10:58:37 +0100

    Change .NET version to 10 Catch and display all exceptions in main.cpp

--- NovaEditor/src/main.cpp
+++ NovaEditor/src/main.cpp
@@ -2,23 +2,34 @@
 #include <Nova/dotnet/Assembly.hpp>
 #include "MainLayer.hpp"
 #include "OverlayLayer.hpp"
+#include <iostream>
 
 int main()
 {
-    Nova::Application::Initialize(
-        Nova::ApplicationSettings{
-            .WindowSettings{
-                .Width = 1080,
-                .Height = 720,
-                .Title = "Nova Editor",
-                .Flags = Nova::WindowFlags::Resizable | Nova::WindowFlags::Vsync},
-            .DotnetSettings{
-                .RuntimeConfigPath = "./assets/Nova.runtimeconfig.json",
-            },
-            .ShaderCacheDirectory = "./shadercache",
-            .TextInputBufferSize = 512});
-    Nova::Application::PushLayer<MainLayer>();
-    Nova::Application::Run();
+    try
+    {
+        Nova::Application::Initialize(
+            Nova::ApplicationSettings{
+                .WindowSettings{
+                    .Width = 1080,
+                    .Height = 720,
+                    .Title = "Nova Editor",
+                    .Flags = Nova::WindowFlags::Resizable | Nova::WindowFlags::Vsync},
+                .DotnetSettings{
+                    .Version = "10.0",
+                    .RuntimeConfigPath = "./assets/Nova.runtimeconfig.json",
+                },
+                .ShaderCacheDirectory = "./shadercache",
+                .TextInputBufferSize = 512});
+        Nova::Application::PushLayer<MainLayer>();
+        Nova::Application::Run();
+    }
+    catch (const std::exception& exc)
+    {
+        std::cerr << exc.what() << std::endl;
+        return 1;
+    }
+    
 
     return 0;
 }
