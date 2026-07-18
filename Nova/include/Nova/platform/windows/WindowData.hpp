af1a68c933d64014b6477009003a7067ef07f8a5
m4reQ <m.mizerski2004@gmail.com>
2026-07-18 23:39:38 +0200

    Store user icon in window data

--- Nova/include/Nova/platform/windows/WindowData.hpp
+++ Nova/include/Nova/platform/windows/WindowData.hpp
@@ -3,14 +3,18 @@
 #include <Nova/platform/windows/WGLContext.hpp>
 #include <Nova/platform/windows/WindowClass.hpp>
 #include <Nova/platform/windows/WindowHandle.hpp>
+#include <Nova/platform/windows/Icon.hpp>
+#include <optional>
 
 namespace Nova
 {
     struct WindowData
     {
+        HINSTANCE instance;
         WindowClass wndClass;
         WindowHandle handle;
         DeviceContext deviceContext;
         WGLContext wglContext;
+        std::optional<Icon> userIcon;
     };
 }
