4842cc05b5e9849991afa24a5a8218e1f87a5323
m4reQ <m.mizerski2004@gmail.com>
2026-07-19 11:37:24 +0200

    Add saved window state for fullscreen transitions

--- Nova/include/Nova/platform/windows/WindowData.hpp
+++ Nova/include/Nova/platform/windows/WindowData.hpp
@@ -5,6 +5,8 @@
 #include <Nova/platform/windows/WindowHandle.hpp>
 #include <Nova/platform/windows/Icon.hpp>
 #include <Nova/platform/windows/Cursor.hpp>
+#include <Nova/platform/windows/WinRect.hpp>
+#include <Nova/graphics/FullscreenMode.hpp>
 #include <optional>
 
 namespace Nova
@@ -18,5 +20,11 @@ namespace Nova
         WGLContext wglContext;
         std::optional<Icon> userIcon;
         std::optional<Cursor> userCursor;
+        WinRect savedRect;
+        FullscreenMode fsMode;
+        DWORD savedStyle;
+        DWORD savedStyleEx;
+        bool shouldClose = false;
+        bool savedMaximizedState = false;
     };
 }
