b5d32a544b2f2e7ccd57ed075340f27f1616430c
m4reQ <m.mizerski2004@gmail.com>
2026-08-08 22:11:10 +0200

    Add profiling and logging initialization code on debug builds

--- Nova/include/Nova/platform/windows/EntryPoint.hpp
+++ Nova/include/Nova/platform/windows/EntryPoint.hpp
@@ -1,6 +1,9 @@
 #include <Nova/core/Application.hpp>
+#include <Nova/debug/Profile.hpp>
+#include <Nova/debug/Log.hpp>
 #include <Nova/platform/windows/StartupData.hpp>
 #include <Nova/platform/windows/DLL.hpp>
+#include <Nova/platform/windows/Error.hpp>
 #include <Windows.h>
 #include <vector>
 #include <string>
@@ -23,30 +26,52 @@ static std::vector<std::string> GetArgs(int argc, char **argv) noexcept
 #ifdef NV_CONSOLE
 int main(int argc, char **argv)
 {
+    // TODO Move this out of main
+    NV_LOG_INITIALIZE("./NovaLog.txt");
+    NV_LOG_INFO("Using working directory \"{}\".", std::filesystem::current_path().string());
+    NV_PROFILE_SET_ENABLED(true);
+    NV_PROFILE_BEGIN_SESSION("./NovaProfileSession.json");
+    NV_PROFILE_SET_CURRENT_THREAD_NAME("MainThread");
+    NV_PROFILE_SET_CURRENT_THREAD_INDEX(0);
+
+#ifndef NV_DEBUG
     try
     {
+#endif
         const Nova::StartupData startupData{
-            .exeInstance = GetModuleHandle(nullptr),
+            .exeInstance = GetModuleHandleA(nullptr),
             .libInstance = Nova::DLL::GetHandle(),
             .args = GetArgs(argc, argv),
-            .showCommand = SW_SHOW,
+            .showCommand = SW_SHOWDEFAULT,
         };
         auto application = CreateApplication(startupData);
         application->Run();
+#ifndef NV_DEBUG
     }
     catch (std::exception e)
     {
         std::cout << e.what() << std::endl;
         return 1;
     }
+#endif
 
     return 0;
 }
 #else
 int APIENTRY WinMain(HINSTANCE hInst, HINSTANCE hInstPrev, PSTR cmdLine, int cmdShow)
 {
+    // TODO Move this out of main
+    NV_LOG_INITIALIZE("./NovaLog.txt");
+    NV_LOG_INFO("Using working directory \"{}\".", std::filesystem::current_path().string());
+    NV_PROFILE_SET_ENABLED(true);
+    NV_PROFILE_BEGIN_SESSION("./NovaProfileSession.json");
+    NV_PROFILE_SET_CURRENT_THREAD_NAME("MainThread");
+    NV_PROFILE_SET_CURRENT_THREAD_INDEX(0);
+
+#ifndef NV_DEBUG
     try
     {
+#endif
         const Nova::StartupData startupData{
             .exeInstance = hInst,
             .libInstance = Nova::DLL::GetHandle(),
@@ -55,12 +80,14 @@ int APIENTRY WinMain(HINSTANCE hInst, HINSTANCE hInstPrev, PSTR cmdLine, int cmd
         };
         auto application = CreateApplication(startupData);
         application->Run();
+#ifndef NV_DEBUG
     }
     catch (std::exception e)
     {
         std::cout << e.what() << std::endl;
         return 1;
     }
+#endif
 
     return 0;
 }
