dd7a14c5a3588e21ad4d00fc991dd5ee3e5e0564
m4reQ <m.mizerski2004@gmail.com>
2026-08-17 00:14:38 +0200

    Make branches inside `check_fits_in` constexpr

--- Nova/include/Nova/core/Memory.hpp
+++ Nova/include/Nova/core/Memory.hpp
@@ -65,9 +65,9 @@ template <typename TDst, typename TSrc>
 constexpr bool check_fits_in([[maybe_unused]] TSrc value) noexcept
 {
 #if NV_DEBUG
-    if (std::numeric_limits<TDst>::is_signed == std::numeric_limits<TSrc>::is_signed)
+    if constexpr (std::numeric_limits<TDst>::is_signed == std::numeric_limits<TSrc>::is_signed)
         return value >= std::numeric_limits<TDst>::min() && value <= std::numeric_limits<TDst>::max();
-    else if (std::numeric_limits<TDst>::is_signed)
+    else if constexpr (std::numeric_limits<TDst>::is_signed)
         return value <= std::numeric_limits<TDst>::max();
     else
         return value >= 0 && value <= std::numeric_limits<TDst>::max();
